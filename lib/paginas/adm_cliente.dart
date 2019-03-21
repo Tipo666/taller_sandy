8f1015066b3add54b86625583ae5dd41fb4e947d
tipo666 <cesarbaez100@gmail.com>
2019-03-21 15:51:12 -0400

    Agregando boton para "Nuevo cliente" y optimizando codigo

--- lib/paginas/adm_cliente.dart
+++ lib/paginas/adm_cliente.dart
@@ -13,14 +13,22 @@ class _AdmClienteState extends State<AdmCliente> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Cliente'),
+      home: Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add, color: Colors.white),
+              tooltip: "Nuevo cliente",
+              onPressed: () => 1,
+            )
+          ],
+          centerTitle: true,
+          title: Text('Cliente'),
         ),
-        body: new SingleChildScrollView(
-          child: new Container(
-            margin: new EdgeInsets.all(15.0),
-            child: new Form(
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(15.0),
+            child: Form(
               key: _key,
               autovalidate: _validate,
               child: FormUI(),
@@ -32,85 +40,99 @@ class _AdmClienteState extends State<AdmCliente> {
   }
 
   Widget FormUI() {
-    return new Column(
+    return Column(
       children: <Widget>[
         new TextFormField(
-          decoration: new InputDecoration(hintText: 'Nombre del cliente'),
+          decoration: InputDecoration(
+              hintText: 'Nombre del cliente', icon: Icon(Icons.person)),
           maxLength: 32,
           validator: validarNombre,
           onSaved: (String val) {
             nombre = val;
           },
         ),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Numero telefonico del cliente'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Numero telefonico del cliente',
+                icon: Icon(Icons.phone)),
             keyboardType: TextInputType.phone,
             maxLength: 10,
             validator: validarTelefono,
             onSaved: (String val) {
               telefono = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Direccion del cliente'),
-            maxLength: 32,
-            validator: validarDireccion,
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Direccion del cliente',
+                icon: Icon(Icons.add_location)),
+            maxLength: 100,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Chasis del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Chasis del auto', icon: Icon(Icons.line_weight)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Color del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Color del auto', icon: Icon(Icons.color_lens)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Ficha del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Ficha del auto',
+                icon: Icon(Icons.insert_drive_file)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Marca del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Marca del auto', icon: Icon(Icons.mode_comment)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Modelo del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Modelo del auto', icon: Icon(Icons.iso)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Placa del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Placa del auto',
+                icon: Icon(Icons.image_aspect_ratio)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-        new TextFormField(
-            decoration: new InputDecoration(hintText: 'Traccion del auto'),
+        TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Traccion del auto', icon: Icon(Icons.drive_eta)),
             maxLength: 32,
-            validator: validarDireccion,
+            validator: validarTexto,
             onSaved: (String val) {
               direccion = val;
             }),
-
-        new SizedBox(height: 15.0),
-        new RaisedButton(
+        SizedBox(height: 15.0),
+        FloatingActionButton(
           onPressed: _sendToServer,
-          child: new Text('Guardar'),
+          tooltip: "Crea el cliente",
+          child: Icon(Icons.save),
         )
       ],
     );
@@ -140,11 +162,10 @@ class _AdmClienteState extends State<AdmCliente> {
     return null;
   }
 
-  String validarDireccion(String value) {
+  String validarTexto(String value) {
     if (value.length == 0) {
-      return "Se requiere la direccion";
-    }
-    else {
+      return "No debe estar vacio";
+    } else {
       return null;
     }
   }
